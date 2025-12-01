8f39b4a25e5b99e5237ba4f0f51d9040914be296
Marl0nico <marlon.nicolalde@epn.edu.ec>
2025-11-30 19:16:49 -0500

    Commit 2

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ import connection from "./database.js";
 
 dotenv.config(); // Configurations
 app.set("port", process.env.PORT || 3000);
-const allowedOrigins = [process.env.URL_FRONTEND, 'http://localhost:5173', 'https://u-connect-epn.vercel.app'];
+const allowedOrigins = [process.env.URL_FRONTEND, 'http://localhost:5173', 'https://front-prueba-sandy.vercel.app/'];
 
 app.use(
   cors({
